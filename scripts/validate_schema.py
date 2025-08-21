5aecf2fabc12c88b99142d90c534b02cda3b8a58
Emily <emily@artyom.me>
2025-08-21 13:48:06 +0200

    license

--- scripts/validate_schema.py
+++ scripts/validate_schema.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env -S mise exec uv@latest -- uv run --script
+# Copyright (c) 2025 Emily
+#
+# This work is licensed under the Creative Commons Zero v1.0 Universal License.
+#
+# To the extent possible under law, the author(s) have dedicated all copyright and related or neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 
 # fmt: off
 #MISE description="Validate SQL schema syntax"
--- src/cnc/utils.py
+++ src/cnc/utils.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025 Emily
+#
+# This work is licensed under the Creative Commons Zero v1.0 Universal License.
+#
+# To the extent possible under law, the author(s) have dedicated all copyright and related or neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 import os
 from typing import Generic, TypeVar
 from datetime import datetime
